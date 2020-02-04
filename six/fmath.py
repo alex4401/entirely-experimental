f5dad128b1569d0fe7a4dda711df8e6e4fe68b04
alex4401 <rylatgl@gmail.com>
2020-02-04 20:59:51 +0100

    [six] update (still too bright)

--- six/fmath.py
+++ six/fmath.py
@@ -1,40 +1,126 @@
 from typing import Tuple
 from math import floor
 
-def lerp(a: float, b: float, alpha: float) -> float:
+Int3 = Tuple[int, int, int]
+Float3 = Tuple[float, float, float]
+
+LUMINANCE = [0.3, 0.59, 0.11]
+
+class _Float3Proxy(object):
+	__slots__ = ('r', 'g', 'b')
+
+	def __init__(self, value: Float3):
+		self.r = value[0]
+		self.g = value[1]
+		self.b = value[2]
+
+def int3_to_float3(pixel: Int3) -> Float3:
+	return (
+		pixel[0] / 255,
+		pixel[1] / 255,
+		pixel[2] / 255,
+	)
+
+def float1_lin_to_srgb(linear: float) -> float:
+	if linear < 0.00313067:
+		return linear * 12.92
+	return (linear ** (1.0 / 2.4)) * 1.055 - 0.055
+
+def float3_lin_to_srgb(linear: Float3) -> Float3:
+	linear = _Float3Proxy(linear)
+	return (
+		float1_lin_to_srgb(linear.r),
+		float1_lin_to_srgb(linear.g),
+		float1_lin_to_srgb(linear.b),
+	)
+
+def float1_srgb_to_lin(color: float) -> float:
+	color = max(6.10352e-5, color)
+	return ((color * (1.0 / 1.055) + 0.0521327) ** 2.4) if color > 0.04045 else color * (1.0 / 12.92)
+
+def float3_srgb_to_lin(color: Float3) -> Float3:
+	color = _Float3Proxy(color)
+	return (
+		float1_srgb_to_lin(color.r),
+		float1_srgb_to_lin(color.g),
+		float1_srgb_to_lin(color.b),
+	)
+
+def float1_lerp(a: float, b: float, alpha: float) -> float:
 	return a * (1 - alpha) + b * alpha
 
-def clamp(min_val: float, value: float, max_val: float) -> float:
+def float3_lerp(a: Float3, b: Float3, alpha: float) -> Float3:
+	a = _Float3Proxy(a)
+	b = _Float3Proxy(b)
+	return (
+		float1_lerp(a.r, b.r, alpha),
+		float1_lerp(a.g, b.g, alpha),
+		float1_lerp(a.b, b.b, alpha),
+	)
+
+def float1_clamp(min_val: float, value: float, max_val: float) -> float:
 	return min(max_val, max(value, min_val))
 
-def get_brightness(color: Tuple[int, int, int]) -> float:
-	return (color[0] * 0.3 + 0.6 * color[1] + 0.1 * color[2]) / (255 * (0.3 + 0.6 + 0.1))
+def float3_clamp(min_val: float, value: Float3, max_val: float) -> Float3:
+	value = _Float3Proxy(value)
+	return (
+		float1_clamp(min_val, value.r, max_val),
+		float1_clamp(min_val, value.g, max_val),
+		float1_clamp(min_val, value.b, max_val),
+	)
 
-def desaturate(color: Tuple[int, int, int], percent: float) -> float:
-	L = get_brightness(color)
-	r = color[0] + percent * (L - color[0])
-	g = color[1] + percent * (L - color[1])
-	b = color[2] + percent * (L - color[2])
-	return (r, g, b)
+def float3_get_luminance(color: Float3) -> float:
+	color = _Float3Proxy(color)
+	Lr = color.r * LUMINANCE[0]
+	Lg = color.g * LUMINANCE[1]
+	Lb = color.b * LUMINANCE[2]
+	return Lr + Lg + Lb
 
-def choose_darker(pixel_a: Tuple[int, int, int], pixel_b: Tuple[int, int, int]) -> Tuple[int, int, int]:
-	La = get_brightness(pixel_a)
-	Lb = get_brightness(pixel_b)
-	if La < Lb:
-		return pixel_a
-	return pixel_b
+def float3_min(a: Float3, b: Float3) -> Float3:
+	a = _Float3Proxy(a)
+	b = _Float3Proxy(b)
+	return (
+		a.r if a.r < b.r else b.r,
+		a.g if a.g < b.g else b.g,
+		a.b if a.b < b.b else b.b,
+	)
 
-def clamp_pixel(lower: int, pixel: Tuple[int, int, int], upper: int) -> Tuple[int, int, int]:
-	return (clamp(lower, pixel[0], upper), clamp(lower, pixel[1], upper), clamp(lower, pixel[2], upper))
+def float3_mul_float1(a: Float3, b: float) -> Float3:
+	a = _Float3Proxy(a)
+	return (
+		a.r * b,
+		a.g * b,
+		a.b * b,
+	)
 
-def to_pixelf(pixel: Tuple[int, int, int]) -> Tuple[float, float, float]:
-	return (pixel[0] / 255, pixel[1] / 255, pixel[2] / 255)
+def float3_mul_float3(a: Float3, b: Float3) -> Float3:
+	a = _Float3Proxy(a)
+	b = _Float3Proxy(b)
+	return (
+		a.r * b.r,
+		a.g * b.g,
+		a.b * b.b,
+	)
 
-def to_pixeli(pixel: Tuple[float, float, float]) -> Tuple[int, int, int]:
-	return clamp_pixel(0, (floor(pixel[0] * 255), floor(pixel[1] * 255), floor(pixel[2] * 255)), 255)
+def float3_add_float3(a: Float3, b: Float3) -> Float3:
+	a = _Float3Proxy(a)
+	b = _Float3Proxy(b)
+	return (
+		a.r + b.r,
+		a.g + b.g,
+		a.b + b.b,
+	)
 
-def sum_pixels(a: Tuple[int, int, int], b: Tuple[int, int, int]) -> Tuple[int, int, int]:
-	return (a[0] + b[0], a[1] + b[1], a[2] + b[2])
+#def desaturate(color: Float3, percent: float) -> float:
+#	L = float3_get_luminance(color)
+#	r = color[0] + percent * (L - color[0])
+#	g = color[1] + percent * (L - color[1])
+#	b = color[2] + percent * (L - color[2])
+#	return (r, g, b)
 
-def mul_pixelf(a: Tuple[float, float, float], b: float):
-	return (a[0] * b, a[1] * b, a[2] * b)
+#def choose_darker(pixel_a: Tuple[int, int, int], pixel_b: Tuple[int, int, int]) -> Tuple[int, int, int]:
+#	La = get_brightness(pixel_a)
+#	Lb = get_brightness(pixel_b)
+#	if La < Lb:
+#		return pixel_a
+#	return pixel_b
--- six/run.py
+++ six/run.py
@@ -7,8 +7,7 @@ from math import floor
 from typing import Tuple
 from PIL import Image
 from tuples import Color3, Color4, ChannelConfig
-from gamma import from_linear_pixelf, from_linear_color
-from fmath import lerp, choose_darker, to_pixelf, to_pixeli, clamp_pixel, sum_pixels, mul_pixelf
+from fmath import *
 
 if len(sys.argv) < 2:
 	print('Usage: six mask diffuse config')
@@ -29,58 +28,91 @@ target_size = mask.size
 
 # Initialize new textures
 colorised = Image.new('RGB', target_size)
-final = Image.new('RGB', target_size)
+final = Image.new('RGBA', target_size)
 
 # Generate channel masks
-def get_single_channel_cmy(texture: Image, decisive_channels: Tuple[int, int]) -> Image:
-	output = Image.new('L', target_size)
+def get_single_channel_cmy(texture: Image, decisive_channels: Tuple[int, int]):
+	output = dict()
 	for x in range(target_size[0]):
 		for y in range(target_size[1]):
 			pixel_in = texture.getpixel((x, y))
-			#pixel_in = from_linear_pixelf(pixel_in)
-			pixel_cmp1 = [pixel_in[index] if index == decisive_channels[0] else 0 for index in range(3)]
-			pixel_cmp2 = [pixel_in[index] if index == decisive_channels[1] else 0 for index in range(3)]
-			#pixel_cmp1 = to_pixeli(pixel_cmp1)
-			#pixel_cmp2 = to_pixeli(pixel_cmp2)
-			pixel_out = choose_darker(pixel_cmp1, pixel_cmp2)
-			output.putpixel((x, y), sum(pixel_out))
+			pixel_in = int3_to_float3(pixel_in)
+			pixel_in = float3_lin_to_srgb(pixel_in)
+			pixel_out = min(pixel_in[decisive_channels[0]], pixel_in[decisive_channels[1]])
+			output[(x, y)] = pixel_out
+	return output
+
+def convert_rgb(texture: Image):
+	output = dict()
+	for x in range(target_size[0]):
+		for y in range(target_size[1]):
+			pixel_in = texture.getpixel((x, y))
+			output[(x, y)] = pixel_in / 255
+	return output
+
+def sub12(a: dict, b: dict):
+	output = dict()
+	for x in range(target_size[0]):
+		for y in range(target_size[1]):
+			v1 = a[(x, y)]
+			v2 = b[(x, y)]
+			output[(x, y)] = float1_clamp(0, v1 - v2, 256)
 	return output
 
 m_red, m_green, m_blue = mask.split()
+rm_red = convert_rgb(m_red)
+rm_green = convert_rgb(m_green)
+rm_blue = convert_rgb(m_blue)
+
+m_red = sub12(rm_red, rm_green)
+m_red = sub12(m_red, rm_blue)
+m_green = sub12(rm_green, rm_red)
+m_green = sub12(m_green, rm_blue)
+m_blue = sub12(rm_blue, rm_red)
+m_blue = sub12(m_blue, rm_green)
 m_cyan = get_single_channel_cmy(mask, (0, 1)) # sic
 m_yellow = get_single_channel_cmy(mask, (1, 2)) # sic
 m_magenta = get_single_channel_cmy(mask, (0, 2))
 
 # Colorise the texture
-def colorise(source: Image, target: Image, info: ChannelConfig):
+def colorise(source: Image, target: dict, info: ChannelConfig):
 	for x in range(target_size[0]):
 		for y in range(target_size[1]):
-			pixel_in = source.getpixel((x, y))
-			L = pixel_in / 255#from_linear_color(pixel_in / 255)
+			pixel_in = source[(x, y)]
 
-			pixel_out = info.target#from_linear_pixelf(info.target)
-			pixel_out = mul_pixelf(pixel_out, info.intensity)
-			pixel_out = mul_pixelf(pixel_out, L)
+			pixel_out = float3_lin_to_srgb(info.target)
+			pixel_out = float3_mul_float1(pixel_out, info.intensity)
+			pixel_out = float3_mul_float1(pixel_out, pixel_in)
 
-			pixel_out = to_pixeli(pixel_out)
-			pixel_out = sum_pixels(target.getpixel((x, y)), pixel_out)
-			pixel_out = clamp_pixel(0, pixel_out, 255)
-			target.putpixel((x, y), pixel_out)
-
-colorise(m_red, colorised, ChannelConfig(**configs['red']))
-colorise(m_green, colorised, ChannelConfig(**configs['green']))
-colorise(m_blue, colorised, ChannelConfig(**configs['blue']))
-colorise(m_cyan, colorised, ChannelConfig(**configs['cyan']))
-colorise(m_yellow, colorised, ChannelConfig(**configs['yellow']))
-colorise(m_magenta, colorised, ChannelConfig(**configs['magenta']))
+			pixel_in = target.get((x, y), (0, 0, 0))
+			target[(x, y)] = float3_add_float3(pixel_in, pixel_out)
+temp = dict()
+colorise(m_red, temp, ChannelConfig(**configs['red']))
+colorise(m_green, temp, ChannelConfig(**configs['green']))
+colorise(m_blue, temp, ChannelConfig(**configs['blue']))
+colorise(m_cyan, temp, ChannelConfig(**configs['cyan']))
+colorise(m_yellow, temp, ChannelConfig(**configs['yellow']))
+colorise(m_magenta, temp, ChannelConfig(**configs['magenta']))
 
 # Blend colorisation with diffuse
+alpha_buf = dict()
 for x in range(target_size[0]):
 	for y in range(target_size[1]):
-		a = diffuse.getpixel((x, y))
-		b = colorised.getpixel((x, y))
-		pixel_out = (floor(a[0] / 255 * b[0]), floor(a[1] / 255 * b[1]), floor(a[2] / 255 * b[2]))
-		colorised.putpixel((x, y), pixel_out)
+		pixel_in1 = diffuse.getpixel((x, y))
+		alpha_buf[(x, y)] = pixel_in1[3]
+		pixel_in1 = int3_to_float3(pixel_in1[:3])
+		pixel_in1 = float3_lin_to_srgb(pixel_in1)
+
+		pixel_in2 = temp[(x, y)]
+
+		temp[(x, y)] = float3_mul_float3(pixel_in1, pixel_in2)
+
+# Copy to texture
+for pos, color in temp.items():
+	color = float3_srgb_to_lin(color)
+	color = float3_mul_float1(color, 255)
+	color = (floor(color[0]), floor(color[1]), floor(color[2]), alpha_buf[pos])
+	final.putpixel(pos, color)
 
 # Save
-colorised.save('out.png')
+final.save('out.png')
